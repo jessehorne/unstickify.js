0676d0bd6ed24a39febae130b6781a55784458e9
Jesse Horne <=>
2014-05-10 15:11:43 -0400

    did some cleanup

--- src/unstickify.js
+++ src/unstickify.js
@@ -2,78 +2,80 @@
 // If it wasn't for that document, it would have taken me a couple more hours to implement this!
 // Buy the guy who wrote that a beer or 8
 
-var css = document.createElement("style");
-css.type = "text/css";
-css.innerHTML = ".drag { position: relative;}"
-document.getElementsByTagName('head')[0].appendChild(css);
+(function() {
+	var css = document.createElement("style");
+	css.type = "text/css";
+	css.innerHTML = ".drag { position: relative;}"
+	document.getElementsByTagName('head')[0].appendChild(css);
 
-var elements = document.querySelectorAll( 'body *' );
-for (var i = 0; i < elements.length; i++) {
-	elements[i].className = " drag";
-	if (elements[i].href) {
-		elements[i].addEventListener("click", function(e) {
-			e.preventDefault();
-		}, false);
-	}
-}
-
-var _startX = 0;
-var _startY = 0;
-var _offsetX = 0;
-var _offsetY = 0;
-var _dragElement;
-var _oldZIndex = 0;
-
-document.onmousedown = OnMouseDown;
-document.onmouseup = OnMouseUp;
-
-function OnMouseDown(e) {
-	// IE
-	if (e == null) {
-		e = window.event;
-	}
-	var target = e.target != null ? e.target : e.srcElement;
-
-	if ((e.button == 1 && window.event != null || e.button == 0) && 
-			target.className.indexOf("drag") > -1) {
-		_startX = e.clientX;
-		_startY = e.clientY;
-		_offsetX = ExtractNumber(target.style.left);
-		_offsetY = ExtractNumber(target.style.top);
-		_oldZIndex = target.style.zIndex;
-		target.style.zIndex = 10000;
-		_dragElement = target;
-		document.onmousemove = OnMouseMove;
-		document.onselectstart = function () { return false; };
-		target.ondragstart = function() { return false; };
-		return false;
-	}
-}
-
-function OnMouseMove(e) {
-	if (e == null) {
-		var e = window.event;
+	var elements = document.querySelectorAll( 'body *' );
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].className = " drag";
+		if (elements[i].href) {
+			elements[i].addEventListener("click", function(e) {
+				e.preventDefault();
+			}, false);
+		}
 	}
 
-	_dragElement.style.left = (_offsetX + e.clientX - _startX) + "px";
-	_dragElement.style.top = (_offsetY + e.clientY - _startY) + "px";
-}
+	var Config = {
+		startX:      0,
+		startY:      0,
+		offsetX:     0,
+		offsetY:     0,
+		dragElement: undefined,
+		oldZIndex:   0
+	};
 
-function OnMouseUp(e) {
-	if (_dragElement != null) {
-		_dragElement.style.zIndex = _oldZIndex;
+	document.onmousedown = OnMouseDown;
+	document.onmouseup = OnMouseUp;
+
+	function OnMouseDown(e) {
+		// IE
+		e = e || window.event;
+
+		var target = e.target || e.srcElement;
+
+		if ((e.button == 1 && window.event != null || e.button == 0) && 
+				target.className.indexOf("drag") > -1) {
+			Config.startX = e.clientX;
+			Config.startY = e.clientY;
+			Config.offsetX = ExtractNumber(target.style.left);
+			Config.offsetY = ExtractNumber(target.style.top);
+			Config.oldZIndex = target.style.zIndex;
+			target.style.zIndex = 10000;
+			Config.dragElement = target;
+			document.onmousemove = OnMouseMove;
+			document.onselectstart = function () { return false; };
+			target.ondragstart = function() { return false; };
+			return false;
+		}
+	}
+
+	function OnMouseMove(e) {
+		if (e == null) {
+			var e = window.event;
+		}
+
+		Config.dragElement.style.left = (Config.offsetX + e.clientX - Config.startX) + "px";
+		Config.dragElement.style.top = (Config.offsetY + e.clientY - Config.startY) + "px";
+	}
+
+	function OnMouseUp(e) {
+		if (Config.dragElement === null) { return; }
+		Config.dragElement.style.zIndex = Config.oldZIndex;
 		document.onmousemove = null;
 		document.onselectstart = null;
-		_dragElement.ondragstart = null;
-		_dragElement = null;
+		Config.dragElement.ondragstart = null;
+		Config.dragElement = null;
 	}
-}
 
-function ExtractNumber(value) {
-	var n = parseInt(value);
-	return n == null || isNaN(n) ? 0 : n;
-}
+	function ExtractNumber(value) {
+		var n = parseInt(value);
+		return n == null || isNaN(n) ? 0 : n;
+	}
 
-function $(id) {
-	return document.getElementById(id);
-}
+	function $(id) {
+		return document.getElementById(id);
+	}
+})();
